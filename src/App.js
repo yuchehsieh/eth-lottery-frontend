280624981087d1ec4f849ee00d1f16abeeb8ba73
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-04-19 19:17:59 +0800

    accessing more properties in the contract

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
 
       this.state = {
         manager: '',
+        players: [],
+        contractBalance: '', /** balance is the big-number, initialize with empty string **/
       }
     }
 
@@ -21,14 +23,23 @@ class App extends Component {
 
       /** when working to metamask provider, don't have to set the "from" property **/
       const manager = await lottery.methods.manager().call();
-      this.setState({ manager });
+      const players = await lottery.methods.getPlayers().call();
+      const contractBalance = await web3.eth.getBalance(lottery.options.address);
+
+      console.log(players, contractBalance);
+
+      this.setState({ manager, players, contractBalance });
     }
 
     render() {
         return (
             <div className="App">
               <h1>Lottery Contract</h1>
-              <p>this contract is managed by: <strong>{this.state.manager}</strong></p>
+              <p>This contract is managed by: <strong>{this.state.manager}</strong></p>
+              <p>
+                There are currently {this.state.players.length} people entered,<br/>
+                competing to win {web3.utils.fromWei(this.state.contractBalance, 'ether')} ether!
+              </p>
             </div>
         );
     }
